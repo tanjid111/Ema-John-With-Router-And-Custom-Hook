e26bffbb74dedf376c66d043efafd8e95fc3a440
tanjid111 <tanjidhossain@gmail.com>
2022-03-30 19:17:10 -0700

    2ndcommit

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products)
+
     return (
         <div>
             <h2>This is order: {products.length}</h2>
+            <p>Cart has: {cart.length}</p>
         </div>
     );
 };
