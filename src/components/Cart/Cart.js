f7a97e32865102bcb6afff17e28ce67d3f78edcb
tanjid111 <tanjidhossain@gmail.com>
2022-03-30 21:27:32 -0700

    final

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import './Cart.css'
 
 const Cart = (props) => {
     const { cart } = props;
-
+    console.log(props);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -24,7 +24,7 @@ const Cart = (props) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
-
+            {props.children}
         </div>
     );
 };
